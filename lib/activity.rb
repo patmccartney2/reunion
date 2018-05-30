02329679eaf500a064665d850a3ab72f75c0088f
Pat McCartney <patmccartney@Pats-MacBook-Pro.local>
2018-05-30 10:58:56 -0600

    through it 1

--- lib/activity.rb
+++ lib/activity.rb
@@ -1,2 +1,32 @@
 class Activity
+  attr_reader :name,
+              :price
+
+  attr_accessor :accounting_roster,
+                :activity_roster,
+                :participant
+
+  def initialize(activity_name, price)
+    @activity_name = name
+    @price = price
+    @accounting_roster = {}
+    @activity_roster = []
+  end
+
+  def gen_hash_with_name_and_price
+    @accounting_roster[@participant] = @price
+  end
+
+  def add_participant(new_participant)
+    @activity_roster << new_participant
+    @accounting_roster[new_participant] = @price
+  end
+
+  def gen_total_cost
+      total_activity_cost = @price / @activity_roster.length
+  end
+
+
+
+
 end
--- test/activity_test.rb
+++ test/activity_test.rb
@@ -4,4 +4,34 @@ require 'minitest/pride'
 require './lib/activity'
 
 class ActivityTest < Minitest::Test
+
+  def test_existence
+    a = Activity.new("skiing", "5")
+    assert_instance_of Activity, a
+  end
+
+  def test_it_has_participants
+    a = Activity.new("skiing", "5")
+    a.add_participant("tom")
+    a.gen_hash_with_name_and_price
+    assert_equal a.accounting_roster["tom"], "5"
+  end
+
+  def test_add_participant
+    a = Activity.new("skiing", "5")
+    a.add_participant("aziz")
+    assert_equal a.activity_roster[0], "aziz"
+  end
+
+  def test_price_calculator
+    a = Activity.new("skiing", 5000)
+    a.add_participant("geo")
+    a.add_participant("theo")
+    a.add_participant("aziz")
+    a.add_participant("tom")
+    a.add_participant("dexter")
+    assert_equal a.gen_total_cost, 1000
+
+  end
+
 end
